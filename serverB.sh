b9c050bee0990376becea875742432a47fdc47b0
chauid <70273749+chauid@users.noreply.github.com>
2022-05-16 05:31:09 +0900

    Update serverB.sh

--- serverB.sh
+++ serverB.sh
@@ -2,11 +2,12 @@
 #1.Pakage Check
 #2.Firewall Check
 #3.Set welcome msg
-#4.vsftpd service start
+#4./etc/vsftpd.conf modify
+#5.vsftpd service start
 echo "[This is ubuntu linux] Auto install"
 echo "Chapter 09 [Nameserver install and operation]"
 echo "Excute in Server(B)"
-echo "Pakage Check(1/4)"
+echo "Pakage Check(1/5)"
 ins_vsftpd=$(dpkg -l vsftpd 2> /dev/null | grep ii | cut -d ' ' -f 1)
 if [ "$ins_vsftpd" == "ii" ]; then
     echo "vsftpd pakage is installed."
@@ -17,17 +18,19 @@ else
     echo "installing vsftpd... (Do not close terminal is installing)"
     apt-get install vsftpd -y vsftpd > /dev/null
 fi
-echo "Firewall Check(2/4)"
+echo "Firewall Check(2/5)"
 ufw allow 21 > /dev/null
-echo "Set welcome msg(3/4)"
+echo "Set welcome msg(3/5)"
 cd /srv/ftp
 if [ ! -f "./welcoome.msg" ]; then
   touch welcome.msg
-  echo "###########################" >> welcome.msg
+  echo "#######################################" >> welcome.msg
   echo "Welcome !!! Ubuntu 20.04 LTS FTP Server" >> welcome.msg
-  echo "###########################" >> welcome.msg
+  echo "#######################################" >> welcome.msg
 fi
-echo "Start vsftpd service(4/4)"
+echo "/etc/vsftpd.conf modify(4/5)"
+sed -i '/^anonymous_enable/s/.*/anonymous_enable=YES\nbanner_file=/srv/ftp/welcome.msg/g' /etc/vsftpd.conf
+echo "Start vsftpd service(5/5)"
 systemctl restart vsftpd > /dev/null
 systemctl enable vsftpd > /dev/null
 StatusFTP=$(systemctl status vsftpd | grep Active | cut -d ' ' -f 7)
