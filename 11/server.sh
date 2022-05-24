d52960240f1b7b4cce2f658fb8c7e5688f1895f1
chauid <70273749+chauid@users.noreply.github.com>
2022-05-24 14:58:56 +0900

    Update Chapter 11

--- 11/server.sh
+++ 11/server.sh
@@ -6,7 +6,7 @@
 #5.mysql 외부 접속 허용 설정
 ins_mariadb_S=$(dpkg -l mariadb-server 2> /dev/null | grep ii | cut -d ' ' -f 1)
 ins_mariadb_C=$(dpkg -l mariadb-client 2> /dev/null | grep ii | cut -d ' ' -f 1)
-echo "패키지 설치 확인(1/)"
+echo "패키지 설치 확인(1/5)"
 if [ "$ins_mariadb_S" == "ii" ]; then
     echo "mariadb-server 설치됨."
 else
@@ -25,7 +25,7 @@ else
     echo "mariadb-client 설치중... (설치중에 터미널을 끄지 마세요.)"
     apt-get install -y mariadb-client > /dev/null
 fi
-echo "#2.mariadb 서비스 시작(2/)"
+echo "#2.mariadb 서비스 시작(2/5)"
 systemctl restart mariadb
 systemctl enable mariadb
 StatusMariaDB=$(systemctl status mariadb | grep Active | cut -d ' ' -f 7) > /dev/null
@@ -35,12 +35,12 @@ else
     echo "오류 : mariadb 서비스가 정상 작동중이 아님."
     exit 0
 fi
-echo "방화벽 설정(3/)"
+echo "방화벽 설정(3/5)"
 ufw allow 3306
-echo "MariaDB 비밀번호 설정(4/)"
+echo "MariaDB 비밀번호 설정(4/5)"
 read -s -p "root권한 비밀번호 : " mariaPW
 mysqladmin -u root password "$mariaPW"
-echo "mysql 외부 접속 허용 설정(5/)"
+echo "mysql 외부 접속 허용 설정(5/5)"
 sed -i '/^bind/s/b/#b/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
 echo "##########자동 설치 완료!##########"
